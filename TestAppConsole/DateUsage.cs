6732a9b9db5f799ef7f1887683a723c0b5b4e23c
hussein-mamane <hous.mamane012@gmail.com>
2024-02-24 09:36:47 +0100

    initial

---
+++ TestAppConsole/DateUsage.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAppConsole
+{
+    public class DateUsage
+    {
+        DateTime now =DateTime.Now;
+    }
+}
